71619e2aab7b709b3d835b8a83c8a7361e41f1b1
Crowdbotics <team@crowdbotics.com>
2020-10-23 12:56:35 +0000

    Update models

--- backend/first_time_ever_21867/wsgi.py
+++ backend/first_time_ever_21867/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_time_ever_21867.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "first_time_ever_21867.settings")
 
 application = get_wsgi_application()
